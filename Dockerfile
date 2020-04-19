71014e4ae21dab7a57c1c99e09c78032ffd96324
Krzysztof Wesolowski <krzysztof.wesolowski@rainlabs.pl>
2020-04-19 23:39:30 +0200

    Rewrite action to python, update cmake in dockerfile
    Add reference actiaon verifications actions:
     - local
     - google test

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,10 @@ FROM ubuntu:18.04
 ENV DEBIAN_FRONTEND=noninteractive
 RUN set -x -e; \
     apt-get update; \
+    apt-get install -y --no-install-recommends apt-transport-https ca-certificates gnupg software-properties-common wget; \
+    wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | apt-key add -; \
+    apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'; \
+    apt-get update; \
     apt-get install -y --no-install-recommends \
         # build
         cmake pkg-config make \
@@ -16,4 +20,4 @@ RUN set -x -e; \
         valgrind
 
 COPY entrypoint.py /usr/local/bin/entrypoint
-ENTRYPOINT ["/usr/bin/python3", "/usr/local/bin/entrypoint"]
+ENTRYPOINT ["/usr/bin/python3", "-u", "/usr/local/bin/entrypoint"]
--- entrypoint.py
+++ entrypoint.py
@@ -1,72 +1,107 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 -u
 
 import subprocess
 import json
-import os
+import os, sys
 
-print(json.dumps(os.environ.copy(), indent=4))
 
-# if [ -n "$INPUT_DEPENDENCIES" ]; then
-#     function install_dependencies {
-#         su - -c "apt-get install --no-install-recommends -y $INPUT_DEPENDENCIES"
-#     }
-# else
-#     function install_dependencies {
-#         :
-#     }
-# fi
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
-# CC="$INPUT_CC"
-# CFLAGS="$INPUT_CFLAGS"
-# CXXFLAGS="$INPUT_CXXFLAGS"
-# if [ -n "${INPUT_COVERAGE}" ]; then
-#     CFLAGS+=" --coverage"
-#     CXXFLAGS+=" --coverage"
-# fi
 
-# ctest=( ctest )
-# if [ -n "$INPUT_CTESTFLAGS" ]; then
-#     ctest+=( "$INPUT_CTESTFLAGS" )
-# fi
+if os.environ["INPUT_VERBOSE"] == "true":
+    verbose = True
+    eprint("Environment: ", json.dumps(os.environ.copy(), indent=4, sort_keys=True))
+else:
+    verbose = False
 
-# set -e -x
 
-# if [ -n "$INPUT_DIRECTORY" ]; then
-#     cd "$INPUT_DIRECTORY"
-# fi
+def check_call(*args, **kwargs):
+    if verbose:
+        eprint("Calling subprocess: ", args)
+    subprocess.check_call(*args, **kwargs)
 
-# if [ -n "$INPUT_ANALYZER" ]; then
-#     if [ "$INPUT_ANALYZER" = "clang-tidy" ]; then
-#         install_dependencies
-#         cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
-#         find "$PWD" \( -name '*.[ch]' -o -name '*.cc' \) -exec clang-tidy -p "$PWD" {} +
-#     elif [ "$INPUT_ANALYZER" = "clang-format" ]; then
-#         find "$PWD" \( -name '*.[ch]' -o -name '*.cc' \) -exec clang-format -i {} +
-#         git diff --exit-code
-#     else
-#         echo "Error: unknown analyzer $INPUT_ANALYZER" >&2
-#         exit 1
-#     fi
-# else
-#     install_dependencies
-#     export CC="$CC"
-#     export CFLAGS="$CFLAGS"
-#     export CXXFLAGS="$CXXFLAGS"
 
-#     SRCDIR="$PWD"
-#     mkdir /tmp/_build /tmp/_install
-#     cd /tmp/_build
-#     cmake -DCMAKE_INSTALL_PREFIX=/tmp/_install "$SRCDIR"
-#     make VERBOSE=1
-#     "${ctest[@]}"
-#     make install
+extra_dependencies = os.environ.get("INPUT_DEPENDENCIES", "").split()
+if len(extra_dependencies) > 0:
+    args = ["apt-get", "install", "--no-install-recommends", "-y"]
+    args.extend(extra_dependencies)
+    check_call(args)
 
-#     if [ -n "${INPUT_COVERAGE}" ]; then
-#         if [ "$INPUT_COVERAGE" = codecov ]; then
-#             bash <(curl -s https://codecov.io/bash)
-#         else
-#             echo "Error: unknown coverage service $INPUT_COVERAGE" >&2
-#             exit 1
-#         fi
-#     fi
-# fi
+callenv = os.environ.copy()
+callenv["CFLAGS"] = callenv.get("CFLAGS", "")
+callenv["CXXFLAGS"] = callenv.get("CXXFLAGS", "")
+
+ctest_coverage = os.environ["INPUT_CTEST_COVERAGE"] == "true"
+
+if ctest_coverage:
+    callenv["CFLAGS"] += " --coverage"
+    callenv["CXXFLAGS"] += " --coverage"
+
+source_directory = os.environ["INPUT_SOURCE_DIRECTORY"]
+build_directory = os.environ["INPUT_BUILD_DIRECTORY"]
+
+if verbose:
+    eprint(
+        "Creating directories: src:",
+        source_directory,
+        ", build: ",
+        build_directory,
+        ", in cwd=",
+        os.getcwd(),
+    )
+
+os.makedirs(os.path.join(os.getcwd(), source_directory), exist_ok=True)
+os.makedirs(os.path.join(os.getcwd(), build_directory), exist_ok=True)
+
+cmake_configure_commands = ["cmake", "-S", source_directory, "-B", build_directory]
+cmake_configure_commands.append("-DCMAKE_EXPORT_COMPILE_COMMANDS=ON")
+
+cmake_configure_sanitize = os.environ["INPUT_CMAKE_CONFIGURE_SANITIZE"]
+if len(cmake_configure_sanitize) > 1:
+    callenv["CFLAGS"] += " -fsanitize=" + cmake_configure_sanitize
+    callenv["CXXFLAGS"] += " -fsanitize=" + cmake_configure_sanitize
+
+cmake_configure_environment = os.environ["INPUT_CMAKE_CONFIGURE_ENVIRONMENT"]
+if verbose:
+    eprint("INPUT_CMAKE_CONFIGURE_ENVIRONMENT=", cmake_configure_environment)
+
+for cenv in cmake_configure_environment.split():
+    if verbose:
+        eprint("Adding entry to call environment:", cenv)
+
+    var, val = cenv.split("=")
+
+    if verbose:
+        eprint("callenv[", var, "] = ", val)
+    callenv[var] = val
+
+cmake_configure_extra_commands = os.environ["INPUT_CMAKE_CONFIGURE_EXTRA_ARGS"].split()
+cmake_configure_commands.extend(cmake_configure_extra_commands)
+
+if verbose:
+    eprint("CMake configure env: ", json.dumps(callenv, indent=4, sort_keys=True))
+check_call(cmake_configure_commands, env=callenv)
+
+if os.environ["INPUT_CMAKE_BUILD_ENABLE"] == "true":
+    cmake_build_options = os.environ.get("INPUT_CMAKE_BUILD_OPTIONS", "").split()
+    args = ["cmake", "--build", build_directory]
+    args.extend(cmake_build_options)
+    check_call(args)
+
+if os.environ["INPUT_CTEST_ENABLE"] == "true":
+    args = ["ctest"]
+    args.extend(os.environ.get("INPUT_CTEST_FLAGS", "").split())
+    check_call(args, cwd=build_directory)
+
+if os.environ["INPUT_CMAKE_INSTALL_ENABLE"] == "true":
+    args = [
+        "cmake",
+        "--install",
+        build_directory,
+        "--prefix",
+        os.environ["INPUT_CMAKE_INSTALL_DIRECTORY"],
+    ]
+    cmake_install_options = os.environ.get("INPUT_CMAKE_INSTALL_OPTIONS", "")
+    args.extend(cmake_install_options)
+    check_call(args)
