9576757fda264f54f8a1060e1ae77fedb33dc05a
Leandro Lisboa Penz <lpenz@lpenz.org>
2025-02-19 22:11:31 +0000

    Fix test_sanitize.py to work with python 3.12

--- tests/test_sanitize.py
+++ tests/test_sanitize.py
@@ -1,31 +1,38 @@
-import imp
+import importlib.machinery
+import types
 import unittest
 
-entrypoint = imp.load_source("entrypoint", "./entrypoint")
+loader = importlib.machinery.SourceFileLoader("entrypoint", "./entrypoint")
+entrypoint = types.ModuleType(loader.name)
+loader.exec_module(entrypoint)
 
 
 class SanitizeTests(unittest.TestCase):
 
     def test_clang(self):
-        uut = entrypoint.Entrypoint({
-            "INPUT_PRESET": "clang-sanitize-address",
-        })
+        uut = entrypoint.Entrypoint(
+            {
+                "INPUT_PRESET": "clang-sanitize-address",
+            }
+        )
         uut.parse()
         self.assertEqual(
             uut.cmd_cmake,
-            'cmake .  -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ '
+            "cmake .  -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ "
             '-DCMAKE_C_FLAGS="-fno-omit-frame-pointer -fsanitize=address" '
-            '-DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer -fsanitize=address"'
+            '-DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer -fsanitize=address"',
         )
 
     def test_gcc(self):
-        uut = entrypoint.Entrypoint({
-            "INPUT_PRESET": "gcc-sanitize-address",
-        })
+        uut = entrypoint.Entrypoint(
+            {
+                "INPUT_PRESET": "gcc-sanitize-address",
+            }
+        )
         uut.parse()
         self.assertEqual(
             uut.cmd_cmake,
-            'cmake .  -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ '
+            "cmake .  -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ "
             '-DCMAKE_C_FLAGS="-fno-omit-frame-pointer -fsanitize=address" '
-            '-DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer -fsanitize=address"'
+            '-DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer -fsanitize=address"',
         )
